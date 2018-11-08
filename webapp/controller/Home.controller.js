21cd0b1232cbcb9e311c48bf1c73493cf14744ee
P2000691080 <sindhum17396@gmail.com>
2018-11-08 06:02:40 +0000

    update

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,49 +1,78 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	'sap/ui/model/json/JSONModel',
+	'sap/viz/ui5/format/ChartFormatter',
+	"sap/ui/core/routing/History"
+
+], function (Controller, JSONModel, ChartFormatter, History) {
 	"use strict";
-
 	return Controller.extend("Mobile.Mobilestore.controller.Home", {
-
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf Mobile.Mobilestore.view.Home
-		 */
-		onInit: function () {
-		
-		},
+		onInit: function () {},
 		onPress: function (oEvent) {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("Inventory");
+		},
+
+		onNavBack: function (oEvent) {
+			var oHistory, sPreviousHash;
+			oHistory = History.getInstance();
+			sPreviousHash = oHistory.getPreviousHash();
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				this.getRouter().navTo("TargetView1", {}, true /*no history*/ );
+			}
+		},
+		OnIconPress: function (oEvent) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			var iconId = oEvent.getParameters().id.substr(12);
+			
+			var rout;
+			switch (iconId) {
+			case "ib":
+				rout = "Billing";
+				break;
+			case "ir":
+				rout = "Recharge";
+				break;
+			case "is":
+				rout = "serviceBilling";
+				break;
+			case "ii":
+				rout = "Inventory";
+				break;
+			case "it":
+				rout = "Transaction";
+				break;
+			}
+			oRouter.navTo(rout);
+		},
+		
+			OnPress: function (oEvent) {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			var iconIdl = oEvent.getParameters().id.substr(12);
+			
+			var routl;
+			switch (iconIdl) {
+			case "ibl":
+				routl = "Billing";
+				break;
+			case "irl":
+				routl = "Recharge";
+				break;
+			case "isl":
+				routl = "serviceBilling";
+				break;
+			case "iil":
+				routl = "Inventory";
+				break;
+			case "itl":
+				routl = "Transaction";
+				break;
+			}
+			oRouter.navTo(routl);
 		}
-	
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf Mobile.Mobilestore.view.Home
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf Mobile.Mobilestore.view.Home
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf Mobile.Mobilestore.view.Home
-		 */
-		//	onExit: function() {
-		//
-		//	}
 
 	});
-
 });
